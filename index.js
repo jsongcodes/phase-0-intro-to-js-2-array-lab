a26d06eb70fdb151a672233b2c06139c5ef87915
Jasmin <ssongnari@gmail.com>
2023-07-31 14:09:25 -0400

    commit tree 2b07c817e72f62bfe8df5cde0a0893e7b72cbab7

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const cats = ["Milo", "Otis", "Garfield"]
+const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name) {
-    return cats.push(name)
-}
+const destructivelyAppendCat = (name) => cats.push(name)
 
-function destructivelyPrependCat(name) {
-    return cats.unshift(name)
-}
+const destructivelyPrependCat = (name) => cats.unshift(name)
 
-function destructivelyRemoveLastCat() {
-    return cats.pop();
-}
+const destructivelyRemoveLastCat = () => cats.pop()
 
 const destructivelyRemoveFirstCat = () => cats.shift()
 
-function appendCat(name) {
-    const copyOfCats = [...cats, name];
-    return copyOfCats
-}
+const appendCat = (name) => [...cats, name]
 
-const prependCat = (name) => {
-    const copyOfCats = [name, ...cats]
-    return copyOfCats
-}
+const prependCat = (name) => [name, ...cats]
 
-const removeLastCat = () => {
-    return cats.slice(0, cats.length - 1)
-}
+const removeLastCat = () => cats.slice(0, - 1)
 
 const removeFirstCat = () => cats.slice(1)
+
